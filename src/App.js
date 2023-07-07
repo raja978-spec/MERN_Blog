a8e5eb27b33f5fc1e97118a50e673477ded52ba9
raja978-spec <rraja48569@gmail.com>
2023-07-07 20:46:06 +0530

    Adding Nav

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import Home from "./pages/Home";
 import Artical from "./pages/Artical";
 import ArticalList from "./pages/ArticalList";
-
+import Navbar from "./components/Nav";
+import About from './pages/About'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar/>
+    <div className="max-w-screen-md mx-auto pt-20">
     <Routes>
       <Route path="/" element={ <Home></Home>}></Route>
       <Route path="/artical-list" element={ <ArticalList></ArticalList>}></Route>
       <Route path="/artical" element={ <Artical></Artical>}></Route>
+      <Route path="/about" element={ <About></About>}></Route>
 
     </Routes>
+    </div>
+    
     </BrowserRouter>
    
   )   
---
+++ src/components/Nav.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Navbar=()=>{
+
+    return(
+        <>
+        <nav className='border-b-4 border-green-700 text-center fixed top-0
+        bg-green-600 font-blod w-full text-lg text-white'>
+          <ul>
+            <li className='inline-block py-4'>
+                <Link to="/artical" className='pl-6 pr-8'>
+                    About
+                </Link>
+                <li className='inline-block py-4'>
+                <Link to="/artical-list" className='pl-6 pr-8'>
+                    Artical List 
+                </Link>
+            </li>
+            </li>
+          </ul>
+        </nav>
+        </>
+    )
+}
+export default Navbar
