9c3118013a2fb8583c7fc7563bf853d0806a8f08
raja978-spec <rraja48569@gmail.com>
2023-07-10 15:32:36 +0530

    Adding Artical list

--- src/pages/Artical.js
+++ src/pages/Artical.js
@@ -4,22 +4,18 @@ import articles from "./Artical-content";
 
 const Artical=()=>{
     const {name}=useParams()
-    var con=""
-    articles.forEach((d)=>{
-        
-        if(d.name===name){
-        con=d.content
-        }
-        else{
-            con="Artical doesn't exist"
-        }
+    var con="Artical cannot be found"
+    articles.map((arr)=>{
+     if(arr.name===name){
+        con=arr.content
+     }
     })
     return(
 <>
 <h1 className="sm:text-4xl text-2xl font-bold my-6 text-center">This is {name} artical</h1>
 <p className="mx-auto leading-relaxed text-base mb-4">
       {con}
-        </p>
+</p>
         
 </>
 
--- src/pages/ArticalList.js
+++ src/pages/ArticalList.js
@@ -1,18 +1,19 @@
 import React from "react";
 import articles from "./Artical-content";
-const ArticalList=()=>{
-    return(
-<>
-{
-articles.map((arr)=>
-{
-<h1 className="sm:text-4xl text-2xl font-bold my-6 text-center">
-{arr.name}
-</h1>
-})
-}
-</>
-    )
-}
+import { Link } from "react-router-dom";
 
-export default ArticalList
+const ArticalList = () => {
+  return (
+    <>
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-center">
+        {articles.map((arr, index) => (
+          <Link to={`/artical/${arr.name}`} key={index}>
+            {arr.name}<br></br>
+          </Link>
+        ))}
+      </h1>
+    </>
+  );
+};
+
+export default ArticalList;
