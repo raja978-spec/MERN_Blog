8a65aebfa9d56ee90933defd8ec188944f0235a7
raja978-spec <rraja48569@gmail.com>
2023-07-13 19:34:06 +0530

    Adding comments route

--- server.js
+++ server.js
@@ -5,5 +5,22 @@ app.use(express.json())
 app.get('/',(req,res)=>{
     res.send("<h1>Hello world</h1>")
 })
+
+const articelcomment={
+    "learn-react":{
+        comment:[],
+    },
+    "learn-node":{
+        comment:[],
+    },
+    "my-thougths-on-node":{
+        comment:[],
+    },
+}
+app.post('/api/:name/artical-comments',(req,res)=>{
+    const {name,comment}=req.body 
+    articelcomment[req.params.name].comment.push({"username":name,"comment":comment})
+    res.status(200).send(articelcomment[req.params.name])
+})
 app.post('/',(req,res)=>res.send(`Hello ${req.body.name}`))
 app.listen(5000,()=>console.log("Server running"))
