5915f3bd662c1394efdba1eaa346c64fb9071b93
raja978-spec <rraja48569@gmail.com>
2023-07-13 19:09:19 +0530

    Adding middleware

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express=require('express')
 app=express()
 
+app.use(express.json())
 app.get('/',(req,res)=>{
     res.send("<h1>Hello world</h1>")
 })
-
+app.post('/',(req,res)=>res.send(`Hello ${req.body.name}`))
 app.listen(5000,()=>console.log("Server running"))
