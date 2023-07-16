3b9bdd6816a7ea577f43c755aceaf9dfcdbd69bf
raja-spec <rraja48569@gmail.com>
2023-07-16 19:14:59 +0530

    Adding Commen component

--- src/pages/Artical.js
+++ src/pages/Artical.js
@@ -3,11 +3,12 @@ import { useParams } from "react-router-dom";
 import articles from "./Artical-content";
 import Articals from "../components/Articals";
 import NotFound from "./404";
+import CommentsList from "../components/Commentslist";
 
 const Artical=()=>{
     const {name}=useParams()
     
-    const {articalInfo,setarticalInfo}=useState({
+    const [articalInfo,setarticalInfo]=useState({
         comments:[]
     });
     // this use effect runs only if name of the
@@ -19,7 +20,7 @@ const Artical=()=>{
             console.log(body)
             setarticalInfo(body)
         }
-        fecthData();
+        fecthData()
      },[name])
 
     var otherArticels="No Articels"
@@ -31,6 +32,7 @@ const Artical=()=>{
 <p className="mx-auto leading-relaxed text-base mb-4">
       {con.content}
 </p>
+
 <h1 className="sm:text-2xl text-xl font-bold my-4 text-gray-900">Other Articels</h1>
       <div className="flex flex-wrap -m-4">
         <Articals article={otherArticels}></Articals>
